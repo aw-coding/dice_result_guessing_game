5b7b8a707ddd572af597ba8a2c4a285f24c5c22c
Andrew Wood <andrewwood90@hotmail.com>
2020-11-16 21:07:45 +0000

    slight change to conditional

--- precourse_recap.py
+++ precourse_recap.py
@@ -10,7 +10,5 @@ print('Guess the result of a dice roll. That\'s any number between 1 and 6.')
 player_answer = int(input('What is your guess?'))
 if player_answer == answer:
     print('Congratulations, you got it right!')
-if player_answer < answer:
-    print('Sorry, your guess was too low.')
-if player_answer > answer:
-    print('Sadly, your guess was too high.')
+else:
+    print('Sadly, your answer was wrong.')
